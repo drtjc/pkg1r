43ed7a4b3ed2566b1241fb9b758f6aa5cea05a96
drtjc <curnock@gmail.com>
2018-09-23 22:53:50 -0500

    add vignettes

---
+++ R/pkg1r.R
@@ -0,0 +1,11 @@
+#' pkg1r: A package for doing nothing.
+#'
+#' The pkgr1 package provides three categories of important functions:
+#' test, test1 and test2.
+#'
+#' @section pkg1r functions:
+#' The pkg1r functions ...
+#'
+#' @docType package
+#' @name pkg1r
+NULL
--- R/test.R
+++ R/test.R
@@ -9,9 +9,58 @@
 #' @examples
 #' test(3L)
 #' test(3.5)
-#'
-#' \dontrun {
-#' test('a')
-#' }
+#' @export
 test <- function(x) x ^ 2
 
+
+#' Constructor
+#'
+#' \code{new_foo} is a constructor for class foo
+#'
+#' @param x numeric
+#'
+#' @return object of class foo
+#'
+#' @examples
+#' nf <- new_foo(3)
+#' @export
+new_foo <- function(x) {
+  stopifnot(is.numeric(x))
+  structure(x, class = "foo")
+}
+
+#' Big time extrovert
+#'
+#' \code{showoff} is a generic
+#'
+#' @param x numeric
+#'
+#' @return show off number
+#'
+#' @examples
+#' nf <- new_foo(3)
+#' @export
+showoff <- function(x) {
+  UseMethod("showoff")
+}
+
+
+#' Big time extrovert for foo
+#'
+#' \code{showoff} is a method for class foo of generic
+#'
+#' @param x numeric
+#'
+#' @return show off number for foo
+#'
+#' @examples
+#' nf <- new_foo(3)
+#' showoff(nf)
+#' @export
+showoff.foo <- function(x) {
+  x ^ 3
+}
+
+# showoff.default <- function(x) {
+#   x ^ 4
+# }
